718a333734923368573f75c92e61746c5a891049
BStauch <varad@posteo.de>
2014-12-16 22:35:15 +0100

    vers1
    Signed-off-by: BStauch <varad@posteo.de>

---
+++ assignment.R
@@ -0,0 +1,2 @@
+training <- read.csv("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv")
+testing <- read.csv("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv")
